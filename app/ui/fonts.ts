6555cd31c34525d02f59cdaad831a87480a72f7f
inoue-nobuhide <inoue-nobuhide@users.noreply.github.com>
2025-04-28 11:24:32 +0900

    6章 vercelにデプロイ

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+import { Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'], // 必要なフォントウェイトを指定
+  subsets: ['latin'], // 必要なサブセットを指定
+});
