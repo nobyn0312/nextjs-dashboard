0973b0cc481208978b27c2c3d21ec012b7294207
inoue-nobuhide <inoue-nobuhide@users.noreply.github.com>
2025-05-07 23:09:58 +0900

    Neon DB 接続

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -62,7 +62,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
-    const data = await Promise.all([
+      const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
       invoiceStatusPromise,
