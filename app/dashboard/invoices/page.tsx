076a6a7b0fc8d30afee014ec02ab32c70a7ea348
井上頌英 <inoueshouei@fuck-mini.local>
2025-05-08 14:33:16 +0900

    pageNation機能

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -16,7 +16,8 @@ export default async function Page(props: {
   const searchParams = await props.searchParams;
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
- 
+  const totalPages = await fetchInvoicesPages(query);
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -30,7 +31,7 @@ export default async function Page(props: {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
