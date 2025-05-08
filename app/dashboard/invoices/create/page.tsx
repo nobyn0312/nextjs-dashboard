8b12ff646ab776bc5c510c50d617de98ce03d307
井上頌英 <inoueshouei@fuck-mini.local>
2025-05-08 16:52:20 +0900

    12賞 invoice CRUD

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,29 @@
+
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+import { createInvoice } from '@/lib/actions';
+
+
+export default async function Page() {
+  const customers = await fetchCustomers();
+
+  return (
+    <>
+    <main>
+    <form action={createInvoice}></form>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+    </>
+  );
+}
